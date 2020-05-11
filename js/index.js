e154494156b9a1b9f4b3885b74efe9b827cc695e
Jiewen Wu <wjw19980321@icloud.com>
2020-05-11 18:05:14 -0400

    files

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+var panorama, viewer, container, infospot;
+
+container = document.querySelector( '#container' );
+
+panorama = new PANOLENS.ImagePanorama('https://raw.githubusercontent.com/jw4590/Twinkling/master/P.jpg');
+
+infospot = new PANOLENS.Infospot( 350, PANOLENS.DataImage.Info );
+infospot.position.set( 5000.00, 3641.87, 108.69 );
+//infospot.addHoverText( 'City God Temple', -50 );
+infospot.addHoverElement(document.getElementById( 'desc-container' ), -100);
+panorama.add( infospot );
+
+viewer = new PANOLENS.Viewer( { container: container } );
+viewer.add( panorama );
+
+
+/* this is used for testing positions
+const panorama = new PANOLENS.ImagePanorama( 'https://raw.githubusercontent.com/jw4590/Twinkling/master/P.jpg' );
+      const viewer = new PANOLENS.Viewer( { output: 'console' } );
+      viewer.add( panorama );
+*/
+
+
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
