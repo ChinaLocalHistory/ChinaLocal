eadea8ba50afa2b9c2326b56a0c900aeb97ad3c6
Jiewen Wu <wjw19980321@icloud.com>
2020-07-12 15:29:05 -0400

    index c

--- panorama/js/index-C.js
+++ panorama/js/index-C.js
@@ -1,4 +1,4 @@
-var pageScene="frontGate"; 
+var pageScene="courtyardNearStage"; 
 const gallery = <ReactGallery/>;
 //const domContainer = document.querySelector('#react-gallery');
 ReactDOM.render(gallery, document.getElementById('react-gallery'));
@@ -9,7 +9,7 @@ ReactDOM.render(gallery, document.getElementById('react-gallery'));
 //generating the panorama
 var v=pannellum.viewer('panorama', {
     "default":{
-        "firstScene":"front-gate",
+        "firstScene":"courtyard-near-stage",
         "author": "JENNY CHANGE THIS",
         "sceneFadeDuration": 1000
     },
